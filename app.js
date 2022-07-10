15238f886a3928fc8ea126f2351d40518aac01ff
Daria Zhuromskaya <zhuromskaya_ds@mail.ru>
2022-07-10 21:31:22 +0700

    fix: edit by comments

--- app.js
+++ app.js
@@ -33,13 +33,13 @@ app.use((req, res, next) => {
   next(new NotFoundError('Страницы не существует'));
 });
 
+app.use(errors());
+
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
   next();
 });
-
-app.use(errors());
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
